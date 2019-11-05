aa095c40e8d4622dfbb0954139cce74c0ce6aea7
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-11-05 09:14:38 -0700

    Add tests for initializing and attributes

--- test/ingredient_test.rb
+++ test/ingredient_test.rb
@@ -4,11 +4,16 @@ require './lib/ingredient'
 
 class IngredientTest < Minitest::Test
   def setup
+    @cheese = Ingredient.new("Cheese", "oz", 50)
   end
 
   def test_it_exists
+    assert_instance_of Ingredient, @cheese
   end
 
   def test_it_initializes_with_readable_attributes
+    assert_equal "Cheese", @cheese.name
+    assert_equal "oz", @cheese.unit
+    assert_equal 50, @cheese.calories
   end
 end
