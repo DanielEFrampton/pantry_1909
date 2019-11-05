76ee8a30fc0a59171cc113ac69c9fdb58de4f7c2
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-11-05 09:37:49 -0700

    Write tests for .stock_check and .restock methods

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -22,4 +22,14 @@ class PantryTest < Minitest::Test
     expected = {}
     assert_equal expected, @pantry.stock
   end
+
+  def test_it_can_check_how_many_of_an_ingredient_are_in_stock
+    assert_equal 0, @pantry.stock_check(@cheese)
+  end
+
+  def test_it_can_restock_given_number_of_given_ingredient
+    @pantry.restock(@cheese, 5)
+    @pantry.restock(@cheese, 10)
+    assert_equal 15, @pantry.stock_check(@cheese)
+  end
 end
